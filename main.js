ae8d224e88b822ce87b9d5b8cf5bb165830f70d7
saminchandeepa <saminchandeepa@gmail.com>
2022-08-09 16:39:06 +0530

    mobile version completed

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-let hamburger = document.getElementById("hamburger");
-let navBarUL = document.getElementById("nav-links");
+// selectors
+
+const hamburger = document.getElementById("hamburger");
+const navBarUL = document.getElementById("nav-links");
+const sliderAmount = document.querySelectorAll(".slider");
+const LeftArrow = document.querySelector("#leftArrow");
+const rightArrow = document.querySelector("#rightArrow");
+
+//user defined
+let counter = 0;
+
+//code for hamburger menu
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -12,3 +22,54 @@ document.querySelectorAll("li").forEach((n) =>
     navBarUL.classList.remove("active");
   })
 );
+
+//code for slider
+
+//functions for slider
+
+//remove all sldes
+function reset() {
+  for (let i = 0; i < sliderAmount.length; i++) {
+    sliderAmount[i].style.display = "none";
+  }
+}
+// toggle the starting slide
+
+function startSlide() {
+  reset();
+  sliderAmount[0].style.display = "block";
+}
+
+//show prev slide
+function prevSlide() {
+  reset();
+  sliderAmount[counter - 1].style.display = "block";
+  counter--;
+}
+//next slide
+
+function nextSlide() {
+  reset();
+  sliderAmount[counter + 1].style.display = "block";
+  counter++;
+}
+
+//left arrow click
+
+LeftArrow.addEventListener("click", function () {
+  if (counter === 0) {
+    counter = sliderAmount.length;
+  }
+  prevSlide();
+});
+
+//right arrow
+
+rightArrow.addEventListener("click", function () {
+  if (counter == sliderAmount.length - 1) {
+    counter = -1;
+  }
+  nextSlide();
+});
+
+startSlide();
